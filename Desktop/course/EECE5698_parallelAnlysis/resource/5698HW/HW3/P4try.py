39d7af6b900f048c72dee4965607d1dd9099215a
zhang-xianl <will.zhangxl@gmail.com>
2017-11-17 17:09:59 -0500

    delete resource

--- Desktop/course/EECE5698_parallelAnlysis/resource/5698HW/HW3/P4try.py
+++
@@ -1,7 +0,0 @@
-import LogisticRegression as LR
-train = LR.readData("newsgroups/news.train")
-test = LR.readData("newsgroups/news.test")
-beta0 = LR.SparseVector({})
-a,b,c = LR.train(train,beta0,0,20,0.001,test)
-
-
